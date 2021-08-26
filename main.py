2d563d5e30db46844b27aa28f4b2236408c38307
Yohannes Getu <nede.evoli@gmail.com>
2021-08-26 13:59:01 +0300

    implemented the News Feed class

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
-class NewsFeed:
+# Api key: 890603a55bfa47048e4490069ebee18c
+import requests
 
-    def __init__(self, data):
-        self.data = data
+
+class NewsFeed:
+    """representing multiple news titles and links as a single string
+    """
+    base_url = "https://newsapi.org/v2/everything?"
+    api_key = "890603a55bfa47048e4490069ebee18c"
+
+    def __init__(self, interest, from_date, to_date, language):
+        self.interest = interest
+        self.from_date = from_date
+        self.to_date = to_date
+        self.language = language
 
     def get(self):
-        pass
+        url = f"{self.base_url}" \
+              f"qInTitle={self.interest}&" \
+              f"from={self.from_date}&" \
+              f"to={self.to_date}&" \
+              f"language={self.language}&" \
+              f"apiKey={self.api_key}"
+        response = requests.get(url)
+        content = response.json()
+        articles = content["articles"]
+
+        email_body = ''
+        for article in articles:
+            email_body += article['title'] + "\n" + article['url'] + "\n\n"
+
+        return email_body
+
+
+news_feed = NewsFeed(interest="nasa",from_date='2021-8-24', to_date='2021-8-24', language='en')
+print(news_feed.get())
