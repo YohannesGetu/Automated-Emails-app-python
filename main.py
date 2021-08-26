7f1441f1b5a0691a258291d771d2112340aa996c
Yohannes Getu <nede.evoli@gmail.com>
2021-08-26 15:14:26 +0300

    implemented the send email

--- main.py
+++ main.py
@@ -1,36 +1,17 @@
-# Api key: 890603a55bfa47048e4490069ebee18c
-import requests
+import yagmail
+import pandas
+from datetime import datetime, timedelta
+from news import NewsFeed
 
+df = pandas.read_excel('people.xlsx')
 
-class NewsFeed:
-    """representing multiple news titles and links as a single string
-    """
-    base_url = "https://newsapi.org/v2/everything?"
-    api_key = "890603a55bfa47048e4490069ebee18c"
+yesterday = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+today = datetime.now().strftime('%Y-%m-%d')
 
-    def __init__(self, interest, from_date, to_date, language):
-        self.interest = interest
-        self.from_date = from_date
-        self.to_date = to_date
-        self.language = language
-
-    def get(self):
-        url = f"{self.base_url}" \
-              f"qInTitle={self.interest}&" \
-              f"from={self.from_date}&" \
-              f"to={self.to_date}&" \
-              f"language={self.language}&" \
-              f"apiKey={self.api_key}"
-        response = requests.get(url)
-        content = response.json()
-        articles = content["articles"]
-
-        email_body = ''
-        for article in articles:
-            email_body += article['title'] + "\n" + article['url'] + "\n\n"
-
-        return email_body
-
-
-news_feed = NewsFeed(interest="nasa",from_date='2021-8-24', to_date='2021-8-24', language='en')
-print(news_feed.get())
+for index, row in df.iterrows():
+    news_feed = NewsFeed(interest=row['interest'], from_date=yesterday, to_date=today)
+    email = yagmail.SMTP(user="pythonprocourse1@gmail.com", password="python_pro_course_1")
+    email.send(to=row['email'],
+               subject=f"Your {row['interest']} news for today!",
+               contents=f"Hi {row['name']}\n See what's on about {row['interest']} today. \n{news_feed.get()} "
+                        f"\nYohannes G")
